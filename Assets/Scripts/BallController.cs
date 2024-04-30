270d9e4dfd7ee18feeabc601d094521c770dd801
ZAIRA ICELA SILVA SANCHEZ <138176414+iceladeveloper@users.noreply.github.com>
2024-04-30 03:29:08 -0600

    4.2. Movimiento en el espacio tridimensional

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,14 +8,29 @@ public class BallController : MonoBehaviour
     public float impulseForce = 3f;
 
     private bool ignoreNextcollision;
+    private Vector3 startPosition;
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
 
 
     private void OnCollisionEnter(Collision collision)
     {
+      
+
+
         if (ignoreNextcollision)
         {
             return;
         }
+
+        DeathPart deathPart =collision.transform.GetComponent<DeathPart>();
+        if (deathPart)
+        {
+            GameManager.singleton.Restartlevel();
+        }        
 
         rb.velocity = Vector3.zero;
         rb.AddForce(Vector3.up*impulseForce, ForceMode.Impulse);
@@ -27,5 +42,9 @@ public class BallController : MonoBehaviour
     private void AllowNextCollision()
     {
         ignoreNextcollision = false;
+    }
+    public void ResetBall()
+    {
+        transform.position = startPosition;
     }
 }
---
+++ Assets/Scripts/HelixController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixController : MonoBehaviour
+{
+
+    private Vector2 lastTapPosition;
+    private Vector3 startPosition;
+    
+    public Transform topTransform;
+    public Transform goalTransform;
+
+    public GameObject helixLevelPrefab;
+    public List<Stage> allStages = new List<Stage>();
+    public float helixDistance;
+
+    private List<GameObject> spawnedLevels = new List<GameObject>();
+
+    private void Awake()
+    {
+        startPosition = transform.localEulerAngles;
+        helixDistance = topTransform.localPosition.y - (goalTransform.localPosition.y + 0.1f);
+        //LoadStage(0); 
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 currentTapPosition = Input.mousePosition;
+            if (lastTapPosition==Vector2.zero)
+            {
+                lastTapPosition = currentTapPosition;
+            }
+
+            float distance = lastTapPosition.x - currentTapPosition.x;
+            lastTapPosition = currentTapPosition;
+            transform.Rotate(Vector3.up * distance);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastTapPosition = Vector2.zero;
+        }
+
+    }
+
+    public void LoadStage(int stageNumber)
+    {
+        
+    }
+}
