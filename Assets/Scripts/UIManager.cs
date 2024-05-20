86a0db370cb9f0568ac31608b6a5bbb43750fbfc
ZAIRA ICELA SILVA SANCHEZ <zaira.silva@udgvirtual.udg.mx>
2024-05-19 21:29:18 -0600

    PI. Publicación del videojuego

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,13 +7,36 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
 
-   public TextMeshProUGUI currentScoreText;
-   public TextMeshProUGUI bestScoreText;
+    public TextMeshProUGUI currentScoreText;
+    public TextMeshProUGUI bestScoreText;
+
+    public Slider slider;
+
+    public TextMeshProUGUI actualLevel;
+    public TextMeshProUGUI nextLevel;
+
+    public Transform topTransform;
+    public Transform goalTransform;
+
+    public Transform ball;
+
+
 
     void Update()
     {
         currentScoreText.text = "Score:" + GameManager.singleton.currentScore;
         bestScoreText.text = "Best: " + GameManager.singleton.bestScore;
+
+    }
+
+    public void ChangeSliderLevelAndProgress()
+    {
+        actualLevel.text = "" + (GameManager.singleton.currentLevel + 1);
+        nextLevel.text = "" + (GameManager.singleton.currentLevel + 2);
+        float totalDistance = (topTransform.position.y - goalTransform.position.y);
+        float distanceLeft = totalDistance - (ball.position.y-goalTransform.position.y);
+        float value = (distanceLeft / totalDistance);
+        slider.value = Mathf.Lerp(slider.value,value,5);
 
     }
 }
